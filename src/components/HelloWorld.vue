6e1c7581adba5c581993f78e7a53de7b9fc674dc
jjinxx <771922509@qq.com>
2019-12-24 19:04:42 +0800

    只写了个轮播

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <div class="nav">
+        <van-swipe :autoplay="3000" indicator-color="white">
+          <van-swipe-item  v-for="item in banners"><img :src="item.pic" alt="" /></van-swipe-item>
+        </van-swipe>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      nav:[],
+      banners:[]
+    }
+  },
+  mounted() {
+    this.getList()
+  },
+  methods:{
+    getList(){
+      let that=this
+      axios.get('http://localhost:3000/banner?type=1')
+        .then(function (response) {
+          console.log(response.data.banners);
+          that.banners=response.data.banners;
+          console.log(that.banners)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.nav img{
+  width: 100%;
+  height: 25vh;
+}
+.nav{
+  background: #000;
+}
+.logo{
+  height: 80%;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
