ed894a77af6ea0ac4b3326fbe387bb7368993927
XuWeijin <778553575@qq.com>
2019-12-29 18:10:11 +0800

    适配

--- src/page/Find.vue
+++ src/page/Find.vue
@@ -5,12 +5,26 @@
         <van-swipe-item  v-for="(item,index) in banners" :key="index"><img :src="item.pic" alt="" /></van-swipe-item>
       </van-swipe>
     </div>
+    <div class="nav-list">
+      <van-row type="flex" justify="space-around">
+        <van-col span="4">
+          <div>
+            <div class="img-wrap"></div>
+            <br/>每日推荐</div>
+        </van-col>
+        <van-col span="4"><div><img src="" alt="歌单"><br/>歌单</div></van-col>
+        <van-col span="4"><div><img src="" alt="排行榜"><br/>排行榜</div></van-col>
+        <van-col span="4"><div><img src="" alt="电台"><br/>电台</div></van-col>
+        <van-col span="4"><div><img src="" alt="直播"><br/>直播</div></van-col>
+      </van-row>
+    </div>
   </div>
 </template>
 
 <script>
-    import request from '../request'
+  import request from '../request'
   import axios from 'axios'
+
   export default {
     name: 'Find',
     data () {
@@ -41,7 +55,7 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
   .nav img{
     width: 100%;
     height: 25vh;
@@ -51,6 +65,25 @@
   }
   .logo{
     height: 80%;
+  }
+  .nav-list{
+    width 750px
+    overflow hidden
+    font-size 12px
+    color #666
+    margin-top 10px
+    border-bottom  1px solid #eee
+    .img-wrap{
+      width 150px
+      height 150px
+      margin 0 auto
+      background #e62619
+    }
+    img{
+      display inline-block
+      width 20px
+      height 20px
+    }
   }
 
   h1, h2 {
