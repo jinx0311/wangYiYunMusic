70bff6b3a7c4ceebd1e518cedd3799cf488116e1
jjinxx <771922509@qq.com>
2020-01-02 19:25:23 +0800

    侧导航

--- src/page/HelloWorld.vue
+++ src/page/HelloWorld.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="hello">
       <van-row class="nav">
-        <van-col style="text-align: left" span="4" @click="showPopup"><van-icon name="orders-o" /></van-col>
-        <ul>
+        <van-col style="text-align: left;padding-top: 10px" span="4" @click="showPopup"><van-icon name="orders-o" size=".6rem" /></van-col>
+        <ul class="topList">
           <li v-for="(item,index,key) in topList" :key="index" @click="handTpId(index)" :class='{active:index==topLiId}'  ><router-link :to="item.url">{{item.title}}</router-link></li>
         </ul>
-        <van-col style="text-align: right" span="4"><van-icon name="search" /></van-col>
+        <van-col style="text-align: right;padding-top: 10px" span="4"><van-icon name="search" size=".6rem" /></van-col>
       </van-row>
       <van-popup class="popupBg" v-model="popup" position="left">
-        <p style="font-size: 0.1rem;margin: 0 2.5%">登陆网易云音乐</p>
-        <p style="font-size: 0.1rem;margin: 0 2.5%">手机电脑多端同步，尽享海量高品质音乐</p>
-        <van-button class="log_button" round type="info">立即登录</van-button>
+        <p style="font-size: 0.1rem;margin: 0 2.5%;text-align: center">登陆网易云音乐</p>
+        <p style="font-size: 0.1rem;margin: 0 2.5%;text-align: center">手机电脑多端同步，尽享海量高品质音乐</p>
+        <van-button class="log_button" round center type="info">立即登录</van-button>
         <van-row class="myList">
           <van-col span="6">
-            <van-icon name="envelop-o" size="1.5rem" color="rgb(246,209,122)" />
+            <van-icon name="envelop-o" size="1rem" color="rgb(255,70,56)" />
             <span>我的消息</span>
           </van-col>
           <van-col span="6">
-            <van-icon name="friends-o" size="1.5rem" color="rgb(246,209,122)"  />
+            <van-icon name="friends-o" size="1rem" color="rgb(255,70,56)"  />
             <span>我的好友</span>
           </van-col>
           <van-col span="6">
-            <van-icon name="service-o" size="1.5rem" color="rgb(246,209,122)"  />
+            <van-icon name="service-o" size="1rem" color="rgb(255,70,56)"  />
             <span>听歌识曲</span>
           </van-col>
           <van-col span="6">
-            <van-icon name="flower-o" size="1.5rem" color="rgb(246,209,122)"  />
+            <van-icon name="flower-o" size="1rem" color="rgb(255,70,56)"  />
             <span>个性装扮</span>
           </van-col>
         </van-row>
@@ -151,7 +151,7 @@
 
       ],
       topLiId:1,
-      popup:false,
+      popup:true,
       list:[
         {
           icon:'video-o',
@@ -190,14 +190,21 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
+  .topList{
+    width: 54%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
   li a{
-    color:#ffffff;
+    color:#000000;
+    background #fff
   }
   .pup_bottom{
     position: absolute;
     bottom: 0;
     width: 100%;
-    background: #000000;
+    background: #ffffff;
     display: flex;
     line-height: 1.2rem;
     justify-content: space-between;
@@ -206,9 +213,9 @@
     padding: 0 0.25%;
     height:100%;
     width: 85%;
-    background: black;
-    color:#ffffff;
-    padding-top: 30px;
+    background: #fff;
+    color:#000;
+    padding-top: 1.2rem;
   }
   .popup_ul1{
     border-top: 1px solid rgba(255,255,255,0.6);
@@ -239,7 +246,9 @@
     margin-top: 20px;
     display: flex;
     flex-direction: column;
-    font-size: 0.5rem;
+    font-size: 0.2rem;
+    justify-content center;
+    align-items center
   }
   .myList{
     margin: 0 2.5%;
@@ -250,12 +259,14 @@
   }
   .log_button{
     background-color: rgba(0,0,0,0);
-    border: 1px solid rgba(255,255,255,0.6);
-    height: 30px;
-    line-height: 30px;
-    font-size: 0.5rem;
-    margin-top: 20px;
+    border: 0.02rem solid #bbb;
+    height: .8rem;
+    color: #000;
+    font-size: 0.4rem;
+    margin-top: 0.266667rem;
     width: 40%;
+    margin: 0.3rem auto;
+    margin-left: 30%;
   }
 .van-overlay{
   background: red !important;
@@ -267,10 +278,10 @@
   font-weight: 800;
 }
 .nav{
-  background: #000;
-  color: #ffffff;
+  background: #fff;
+  color: #000;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   padding: 8px 2.5%;
   font-size: 28px;
